37c9e207307d782e4d1467bbd2ac3eaab05b9fb2
warren Grege <waren23.greg@student.cuk.ac.ke>
2026-04-28 15:31:49 +0300

    feat: add firebase-config.js with correct Firebase config

---
+++ firebase-config.js
@@ -0,0 +1,12 @@
+// firebase-config.js
+const firebaseConfig = {
+  apiKey: "AIzaSyB_9MvliXbsjsIh52kMSMlInckTa0kXGOY",
+  authDomain: "nestfinder-cuk.firebaseapp.com",
+  databaseURL: "https://nestfinder-cuk-default-rtdb.firebaseio.com",
+  projectId: "nestfinder-cuk",
+  storageBucket: "nestfinder-cuk.firebasestorage.app",
+  messagingSenderId: "508247387540",
+  appId: "1:508247387540:web:473ceb9425182fa0fe3301",
+  measurementId: "G-VFLY44TEX1"
+};
+const supabase = createClient(firebaseConfig);
