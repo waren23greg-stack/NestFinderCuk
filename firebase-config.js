8283ef210deb9ffdb043c9deb1d44c756e8dfdae
warren Grege <waren23.greg@student.cuk.ac.ke>
2026-04-29 15:39:30 +0300

    fix: clean firebase-config no corruption

--- firebase-config.js
+++ firebase-config.js
@@ -1,13 +1,12 @@
 // firebase-config.js
 const firebaseConfig = {
-  apiKey: "AIzaSyB_9MvliXbsjsIh52kMSMlInckTa0kXGOY",
-  authDomain: "nestfinder-cuk.firebaseapp.com",
-  databaseURL: "https://nestfinder-cuk-default-rtdb.firebaseio.com",
-  projectId: "nestfinder-cuk",
-  storageBucket: "nestfinder-cuk.firebasestorage.app",
-  messagingSenderId: "508247387540",
-  appId: "1:508247387540:web:473ceb9425182fa0fe3301",
-  measurementId: "G-VFLY44TEX1"
+  apiKey: 'AIzaSyB_9MvliXbsjsIh52kMSMlInckTa0kXGOY',
+  authDomain: 'nestfinder-cuk.firebaseapp.com',
+  databaseURL: 'https://nestfinder-cuk-default-rtdb.firebaseio.com',
+  projectId: 'nestfinder-cuk',
+ storageBucket: 'nestfinder-cuk.firebasestorage.app',
+  messagingSenderId: '508247387540',
+  appId: '1:508247387540:web:473ceb9425182fa0fe3301',
+  measurementId: 'G-VFLY44TEX1'
 };
-
 firebase.initializeApp(firebaseConfig);
