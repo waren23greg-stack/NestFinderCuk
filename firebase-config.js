82919d5585df8949c1c01153057ca24dcf36db41
warren Grege <waren23.greg@student.cuk.ac.ke>
2026-04-30 17:38:44 +0300

    fix: clean firebase-config.js corrupted authDomain and storageBucket

--- firebase-config.js
+++ firebase-config.js
@@ -4,7 +4,7 @@ const firebaseConfig = {
   authDomain: 'nestfinder-cuk.firebaseapp.com',
   databaseURL: 'https://nestfinder-cuk-default-rtdb.firebaseio.com',
   projectId: 'nestfinder-cuk',
- storageBucket: 'nestfinder-cuk.firebasestorage.app',
+  storageBucket: 'nestfinder-cuk.firebasestorage.app',
   messagingSenderId: '508247387540',
   appId: '1:508247387540:web:473ceb9425182fa0fe3301',
   measurementId: 'G-VFLY44TEX1'
